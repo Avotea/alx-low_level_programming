431633fad993ddcf264a7a84185eaa6d5f9e7de2
Thembelihle Shandu <thembelihleshandu93@gmail.com>
2022-10-14 03:22:41 -0700

    betty corr

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,26 +1,19 @@
+/*
+ * File: 4-puts.c
+ * Auth: Brennan D Baraban <375@holbertonschool.com>
+ */
+
 #include <stdio.h>
 
-
-
 /**
- *  * main - Entry point
+ * main - Prints "Programming is like building a multilingual
+ * puzzle, followed by a new line.
  *
- *   *
- *
- *    * Description: prints "Programming is like building a multilingual puzzle"
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *       */
-
+ * Return: Always 0.
+ */
 int main(void)
-
 {
+	puts("\"Programming is like building a multilingual puzzle");
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	return (0);
 }
