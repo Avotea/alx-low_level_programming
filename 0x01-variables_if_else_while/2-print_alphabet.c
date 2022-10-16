8b6877bae377aeb2d2a3d354dad0fcd37211c75b
Thembelihle Shandu <thembelihleshandu93@gmail.com>
2022-10-16 07:08:31 -0700

    sdhd

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase, and then in uppercase.
+ * main - Prints the alphabet in lowercase.
  *
  * Return: Always 0.
  */
@@ -9,7 +9,7 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'z'; letter >= 'a'; letter--)
 		putchar(letter);
 
 	putchar('\n');
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - Prints the lowercase alphabet in reverse.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char letter;
+	char low;
 
-	for (letter = 'z'; letter >= 'a'; letter--)
-		putchar(letter);
-	
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
 	putchar('\n');
 
 	return (0);
 }
+
