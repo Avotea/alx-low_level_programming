eef86141c8766e1ce79406434c301bfbf456ec9c
Thembelihle Shandu <thembelihleshandu93@gmail.com>
2022-10-19 13:09:10 -0700

    yghhdw

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
-#include "holberton.h"
+#include <unistd.h>
 
 /**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
- *\
- void print_alphabet(void)
- {
- char letter;
-
- for (letter = 'a'; letter <= 'z'; letter++)
- _putchar(letter);
+ *  _putchar - writes the character c to stdout
+ *  @c: The character to print
+ *
+ *  Return: On success 1.
+ *  On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
